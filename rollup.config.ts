5aca8cd6b1e00f480fb5df0c4fc3e14f361e008c
Yury Rivera <yrivera@applaudostudios.com>
2023-11-05 22:02:44 -0600

    tracking setup files

---
+++ rollup.config.ts
@@ -0,0 +1,23 @@
+import commonjs from '@rollup/plugin-commonjs';
+import typescript from '@rollup/plugin-typescript';
+import resolve from '@rollup/plugin-node-resolve';
+import { RollupOptions } from 'rollup';
+
+const config: RollupOptions = {
+  input: 'src/index.ts',
+  output: {
+    dir: 'dist',
+    format: 'es',
+    sourcemap: true,
+    preserveModules: true,
+    exports: 'named',
+    assetFileNames: '[name][extname]',
+  },
+  plugins: [
+    resolve({ preferBuiltins: true }),
+    commonjs(),
+    typescript({ tsconfig: './tsconfig.json' }),
+  ],
+};
+
+export default config;
