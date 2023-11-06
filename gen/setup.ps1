2a733a4af5b55892a6d7c83d3980eff5cfca633f
Yury Rivera <yrivera@applaudostudios.com>
2023-11-05 22:26:17 -0600

    adding more setup files

---
+++ gen/setup.ps1
@@ -0,0 +1,20 @@
+﻿$libDir = ".\grammar\"
+$pack = ".\src\parser\"
+$ourDir = ".\src\parser\"
+
+if (Test-Path $ourDir) {
+    Write-Host "Folder Exists"
+}
+else
+{
+    New-Item $ourDir -ItemType Directory
+    Write-Host "Folder Created successfully"
+}
+
+Remove-Item "$ourDir*" -Recurse -Force
+
+java -jar gen\antlr.jar -lib $libDir -Dlanguage=TypeScript -encoding UTF8 -Xexact-output-dir -package $pack -o $libDir .\grammar\HaibtLexer.g4
+
+java -jar gen\antlr.jar -lib $libDir -Dlanguage=TypeScript -encoding UTF8 -Xexact-output-dir -package $pack -o $ourDir -visitor .\grammar\Haibt.g4
+
+Move-Item $libDir\HaibtLexer.ts -Destination $ourDir -Force
