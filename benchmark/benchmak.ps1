8fce6e3c7034c415448aac54f67ef4be000b5bec
Yury Rivera <yrivera@applaudostudios.com>
2024-02-06 20:23:24 -0600

    updating benchmak.ps1

--- benchmark/benchmak.ps1
+++ benchmark/benchmak.ps1
@@ -1,35 +1,45 @@
 param (
-  [Parameter(Position=0, Mandatory=$true)][string]$fPath,
-  [Parameter(Position=1)][int]$repetitions = 10,
-  [Parameter(Position=2)][switch]$generation = $true
+  [Parameter(Position=0)][int]$repetitions = 10,
+  [Parameter(Position=1)][switch]$generation = $true
 )
 
-$file_name = [System.IO.Path]::GetFileName($fPath)
-Write-Host $PWD
-Write-Host "Test :" $file_name
-Write-Host "Generation :" $generation
-Write-Host "Reps :" $repetitions
-$fileNameOnly = [System.IO.Path]::GetFileNameWithoutExtension($fPath)
-
-$outFile = "results\$fileNameOnly.json"
-
-"{`r`n  `"file`": `"$file_name`",`r`n  `"runs`": [`r`n" | Out-File -Encoding utf8  -FilePath $outFile -Force
-
-for ($i = 0; $i -lt $repetitions; $i++) {
-  Write-Host "Run $i started"
-  $uptime = Get-Date
-  if ($generation) {
-    crossbind -p react -l perf $fPath | Out-File -Encoding utf8 -FilePath $outFile -Append
-  } else {
-    crossbind -p react -l perf --no-generation $fPath | Out-File -Encoding utf8 -FilePath $outFile -Append
-  }
-  if ($i -lt ($repetitions - 1)) {
-      "," | Out-File -Encoding utf8 -FilePath $outFile -Append
-  }
-  $downtime = Get-Date
-  Write-Host "Run $i took " ($downtime - $uptime).TotalSeconds " seconds"
+Function MakeName([string]$path) {
+  $fileName = [System.IO.Path]::GetFileName($fPath)
+  Write-Host "Test :" $fileName
+  $out = "results\$fileName.json"
+  "{`r`n  `"file`": `"$fileName`",`r`n  `"runs`": [`r`n" | Out-File -Encoding utf8  -FilePath $out -Force
+  return $out
 }
 
-"  ]`r`n}" | Out-File -Encoding utf8 -FilePath $outFile -Append
+Function Bench([string]$out, [string]$path, [switch]$gen = $true) {
+  if ($gen) {
+    crossbind -p react -l perf $path | Out-File -Append -Encoding utf8 -FilePath $out
+  } else {
+    crossbind -p react -l perf --no-generation $path | Out-File -Append -Encoding utf8 -FilePath $out
+  }
+}
 
+Function Runs([string]$fPath, [int]$repetitions, [switch]$generation) {
+ $out = MakeName $fPath
+ for ($i = 0; $i -lt $repetitions; $i++) {
+   Write-Host "Run $i started"
+   $uptime = Get-Date
+   Bench $out $fPath $generation
+   if ($i -lt ($repetitions - 1)) {
+     "," | Out-File -Encoding utf8 -FilePath $out -Append
+   }
+   $downtime = Get-Date
+   Write-Host "Run $i took " ($downtime - $uptime).TotalSeconds " seconds"
+ }
+ "  ]`r`n}" | Out-File -Encoding utf8 -FilePath $out -Append
+}
+
+Function LinearTest([int]$repetitions, [switch]$generation) {
+  for ($i = 50; $i -lt 864; $i = $i + 50) {
+     Runs "test/c1-p0-n$i-l0-a0.hbt" $repetitions $generation
+  }
+  Runs "test/c1-p0-n864-l0-a0.hbt" $repetitions $generation
+}
+
+LinearTest $repetitions $generation
 
--- benchmark/linear.ps1
+++ benchmark/linear.ps1
@@ -4,7 +4,7 @@ param (
 )
 
 for ($i = 50; $i -lt 864; $i = $i + 50) {
-  & ".\benchmak.ps1" -repetitions $repetitions -generation:$generation -fPath "test/c1-p0-n1-n$i-l0-a0.hbt"
+  & ".\benchmak.ps1" -repetitions $repetitions -generation:$generation -fPath
 }
 
 & ".\benchmak.ps1" -repetitions $repetitions -generation:$generation -fPath "test/c1-p0-n1-n864-l0-a0.hbt"
